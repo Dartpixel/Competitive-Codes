c06bd2e4ea5b70a2b973e21ff17c4b84baa8aa23
Hardik Sharma <shardik2001@gmail.com>
2023-03-29 23:16:04 +0530

    Updated

---
+++ CODECHEF/COUGAME.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int testcase;
+	cin>>testcase;
+	while(testcase--)
+	{
+	    int a,b;
+	    cin>>a>>b;
+	    cout<<(b-a)<<endl;
+	}
+	return 0;
+}
